deaa34af834d82a3b3952a1b166ed169bac8b7be
CAhyun <132358890+AhyunChae@users.noreply.github.com>
2025-09-04 14:51:19 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+num1 = int(input());
+num2 = input();
+
+procc1 = num1 * int(num2[2]);
+procc2 = num1 * int(num2[1]);
+procc3 = num1 * int(num2[0]);
+
+print(procc1);
+print(procc2);
+print(procc3);
+print(num1 * int(num2));
